ef23d16644d452b67b23b18e499648e456f47957
geassyah <79988610+geassyah@users.noreply.github.com>
2021-08-20 08:10:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,5 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/ysc3839/openwrt-minieap.git -b gzhu /packages/minieap
+git clone https://github.com/ysc3839/openwrt-minieap.git -b gzhu /package/minieap
  
